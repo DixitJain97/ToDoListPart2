7c567f7622d83764c61aab0c2ade462864e815e8
DixitJain97 <59871037+DixitJain97@users.noreply.github.com>
2020-12-07 04:54:40 +1030

    Comitting after making first version of all files

---
+++ script.js
@@ -0,0 +1,83 @@
+const inputElement = document.querySelectorAll("input");
+const saveButton = document.querySelector(".to-do-button");
+const clearButton = document.querySelector(".clear-all");
+const labels = document.querySelectorAll('label');
+let savedText="";
+
+let checkTime = ()=>{
+    let currentTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+    for(let i=0;i<labels.length;i++){
+        if(currentTime.slice(-2) === labels[i].innerText.slice(-2)){
+            if(parseInt(currentTime.substr(0,2)) > parseInt(labels[i].innerText.substr(0,2))){
+                inputElement[i].setAttribute("style", "background-color: #fdd1d2;")
+            }
+            else if(parseInt(currentTime.substr(0,2)) === parseInt(labels[i].innerText.substr(0,2))){
+                inputElement[i].setAttribute("style", "background-color: #fbede0;")
+            }
+            else{
+                if(parseInt(labels[i].innerText.substr(0,2)) === 12){
+                    inputElement[i].setAttribute("style", "background-color: #fdd1d2;")
+                }
+                else{
+                    inputElement[i].setAttribute("style", "background-color: #96d4cc;")
+                }
+                
+            }
+        }
+        else{
+            
+            if(currentTime.slice(-2)==="PM" &&  labels[i].innerText.slice(-2) === "AM"){
+                inputElement[i].setAttribute("style", "background-color: #fdd1d2;")
+            }
+            
+            else{
+                    inputElement[i].setAttribute("style", "background-color: #96d4cc;")
+            }
+            
+        }
+        
+    }
+}
+
+checkTime();
+
+let updateDisplay = ()=>{
+    if(sessionStorage.getItem('savedTasks') != null){
+        let toDoItems =sessionStorage.getItem('savedTasks').split(',');
+        for(let i=0;i<toDoItems.length;i++){
+            inputElement[i].value = toDoItems[i];
+        }
+    }
+    else{
+        for(let i=0;i<inputElement.length;i++){
+            inputElement[i].value = "";
+        }
+    }
+}
+
+let saveData = ()=>{
+    inputElement.forEach(element => {
+        if(savedText === ""){
+            savedText = element.value ;
+        }
+        else{
+            savedText = savedText + "," + element.value;
+        }
+    });
+    sessionStorage.setItem('savedTasks',savedText);
+}
+
+saveButton.addEventListener('click',()=>{
+    saveData();
+    updateDisplay();
+})
+
+updateDisplay();
+
+clearButton.addEventListener('click',()=>{
+    savedText = "";
+    sessionStorage.setItem('savedTasks',savedText);
+    updateDisplay();
+    location.reload();
+})
+
