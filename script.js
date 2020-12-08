3991e8077d9d8c6adcd8a236a9c9c758cc8437f3
DixitJain97 <59871037+DixitJain97@users.noreply.github.com>
2020-12-08 12:58:49 +1030

    Adding comments and making changes to check time function

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+// getting instances of different elements
 const inputElement = document.querySelectorAll("input");
 const saveButton = document.querySelector(".to-do-button");
 const clearButton = document.querySelector(".clear-all");
 const labels = document.querySelectorAll('label');
 let savedText="";
 
+// building function to check times and set bg-color for labels
 let checkTime = ()=>{
     let currentTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
     for(let i=0;i<labels.length;i++){
         if(currentTime.slice(-2) === labels[i].innerText.slice(-2)){
             if(parseInt(currentTime.substr(0,2)) > parseInt(labels[i].innerText.substr(0,2))){
-                inputElement[i].setAttribute("style", "background-color: #fdd1d2;")
+                if(parseInt(currentTime.substr(0,2)) === 12){
+                    inputElement[i].setAttribute("style", "background-color: #96d4cc;")
+                }else{
+                    inputElement[i].setAttribute("style", "background-color: #fdd1d2;")
+                }
             }
             else if(parseInt(currentTime.substr(0,2)) === parseInt(labels[i].innerText.substr(0,2))){
                 inputElement[i].setAttribute("style", "background-color: #fbede0;")
@@ -41,6 +47,7 @@ let checkTime = ()=>{
 
 checkTime();
 
+//function for updating displays
 let updateDisplay = ()=>{
     if(sessionStorage.getItem('savedTasks') != null){
         let toDoItems =sessionStorage.getItem('savedTasks').split(',');
@@ -55,6 +62,7 @@ let updateDisplay = ()=>{
     }
 }
 
+//function for saving data
 let saveData = ()=>{
     inputElement.forEach(element => {
         if(savedText === ""){
@@ -67,6 +75,7 @@ let saveData = ()=>{
     sessionStorage.setItem('savedTasks',savedText);
 }
 
+//defining on-click functions
 saveButton.addEventListener('click',()=>{
     saveData();
     updateDisplay();
